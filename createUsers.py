9a31187b4f55e5bad99b8d916774cc0769df30fa
austinspeedy11 <67382354+austinspeedy11@users.noreply.github.com>
2020-11-11 15:36:12 -0600

    Update createUsers.py

--- createUsers.py
+++ createUsers.py
@@ -16,30 +16,30 @@ user = input("Please enter your Jamf Pro API user's username: ")
 password = input("Please enter your Jamf Pro API user's password: ")
 instanceName = input("Please enter your Jamf Pro instance name: ")
 
-sheetIDs = []
-for i in range(sheet.nrows -1):
-		sheetIDs.append(int((sheet.cell_value(i + 1, 0))))
-		
+#sheetIDs = []
+#for i in range(sheet.nrows -1):
+#	sheetIDs.append(int((sheet.cell_value(i + 1, 0))))
+
 sheetNames = []
 for i in range(sheet.nrows -1):
-	sheetNames.append((sheet.cell_value(i + 1, 1)))
-	
+	sheetNames.append((sheet.cell_value(i + 1, 0)))
+		
 sheetFullNames = []
 for i in range(sheet.nrows -1):
-	sheetFullNames.append((sheet.cell_value(i + 1, 2)))
-
+	sheetFullNames.append((sheet.cell_value(i + 1, 1)))
+	
 sheetEmails = []
 for i in range(sheet.nrows -1):
-	sheetEmails.append((sheet.cell_value(i + 1, 3)))
-		
+	sheetEmails.append((sheet.cell_value(i + 1, 2)))
+
 sheetPhoneNumbers = []
 for i in range(sheet.nrows -1):
-	sheetPhoneNumbers.append(int(sheet.cell_value(i + 1, 4)))
-	
+	sheetPhoneNumbers.append((sheet.cell_value(i + 1, 3)))
+		
 sheetPositions = []
 for i in range(sheet.nrows -1):
-	sheetPositions.append((sheet.cell_value(i + 1, 5)))
-
+	sheetPositions.append(sheet.cell_value(i + 1, 4))
+	
 for i in range(sheet.nrows -1):
 	resp = requests.post("https://" + instanceName + ".jamfcloud.com/JSSResource/users/id/0", auth=(user, password), headers={"content-Type": "text/xml"}, data="<user><id>0</id><name>" + sheetNames[i] + "</name><full_name>" + sheetFullNames[i] + "</full_name><email>" + sheetEmails[i] + "</email><email_address>aharrison@company.com</email_address><phone_number>" + str(sheetPhoneNumbers[i]) + "</phone_number><position>" + sheetPositions[i] + "</position><sites><site><id>-1</id><name>None</name></site></sites></user>")
 
